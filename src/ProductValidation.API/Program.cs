5f6d11b9a542984a742e42194065dc67e95f86e2
acikgozb <acikgozberk96@gmail.com>
2022-07-05 23:54:56 +0300

    Disable ApiController auto validation for non-nullable fields
    The goal here is to use FluentValidation instead of auto validation done by ApiController attribute. Therefore this option is disabled.

--- src/ProductValidation.API/Program.cs
+++ src/ProductValidation.API/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+});
 
 var app = builder.Build();
 
