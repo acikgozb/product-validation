d4553d0d0660e9a06a429f132f2f565fb69a32c8
acikgozb <acikgozberk96@gmail.com>
2022-07-08 23:13:16 +0300

    Add ProductValidationService and custom DtoValidator to DI container

--- src/ProductValidation.API/Program.cs
+++ src/ProductValidation.API/Program.cs
@@ -1,8 +1,19 @@
+using FluentValidation;
+using ProductValidation.Core.Contracts;
+using ProductValidation.Core.Dtos;
+using ProductValidation.Core.Services;
+using ProductValidation.Core.Validators;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers(options =>
 {
     options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
 });
+
+builder.Services.AddScoped<IModelValidator, ProductValidationService>();
+builder.Services.AddScoped<IValidator<ProductRequestDto>, ProductRequestDtoValidator>();
+
 
 var app = builder.Build();
 
