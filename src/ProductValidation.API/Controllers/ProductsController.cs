0b5db05f6c89d125a9bb7b06d6bde73c266e5448
acikgozb <acikgozberk96@gmail.com>
2022-07-08 23:13:59 +0300

    Inject IModelValidator in ProductsController to validate dto.

--- src/ProductValidation.API/Controllers/ProductsController.cs
+++ src/ProductValidation.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProductValidation.Core.Contracts;
 using ProductValidation.Core.Dtos;
 
 namespace ProductValidation.API.Controllers
@@ -7,6 +8,14 @@ namespace ProductValidation.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IModelValidator _modelValidator;
+
+
+        public ProductsController(IModelValidator modelValidator)
+        {
+            _modelValidator = modelValidator;
+        }
+
         [HttpGet]
         public IActionResult GetProducts()
         {
@@ -22,11 +31,10 @@ namespace ProductValidation.API.Controllers
              * ProductRequestDto
              * Product Entity
              */
-            
             /*
              * If validation passes, map DTO to entity and save it to DB.
              */
-            
+            _modelValidator.Validate(productRequestDto);
             throw new NotImplementedException();
         }
         
