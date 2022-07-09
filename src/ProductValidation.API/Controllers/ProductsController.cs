add27c5cb07f70828bf572466c7cd079947710ca
acikgozb <acikgozberk96@gmail.com>
2022-07-10 00:33:38 +0300

    Add DTO validation to AddProduct action method
    Model binding only validates types, the rest should be validated manually in each endpoint.

--- src/ProductValidation.API/Controllers/ProductsController.cs
+++ src/ProductValidation.API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProductValidation.Core.Contracts;
+using ProductValidation.Core.Models;
 using ProductValidation.Core.Models.Dtos;
 
 namespace ProductValidation.API.Controllers;
@@ -9,7 +10,7 @@ namespace ProductValidation.API.Controllers;
 public class ProductsController : ControllerBase
 {
     private readonly IModelValidator _modelValidator;
-    
+
     public ProductsController(IModelValidator modelValidator)
     {
         _modelValidator = modelValidator;
@@ -22,12 +23,25 @@ public class ProductsController : ControllerBase
         throw new NotImplementedException();
     }
 
-
-    //If validation passes, map DTO to entity and save it to DB.
     [HttpPost]
     [Consumes("application/json")]
     public IActionResult AddProduct(ProductRequestDto productRequestDto)
     {
+        var validationResult = _modelValidator.Validate(productRequestDto);
+        if (!validationResult.IsValid)
+        {
+            var fieldValidationResult = validationResult.Errors.ConvertAll(validationFailure =>
+                new FieldValidationResult
+                {
+                    FieldName = validationFailure.PropertyName,
+                    ErrorMessage = validationFailure.ErrorMessage
+                });
+
+            return BadRequest(fieldValidationResult);
+        }
+        
+        //Valid DTO model -> call service to handle business logic.
+
         throw new NotImplementedException();
     }
 }
