399d101ee44e1fbec9dd3bffcfc83661f27106ee
acikgozb <acikgozberk96@gmail.com>
2022-07-09 22:48:00 +0300

    Move ProductRequestDto under Models/ and make all fields nullable
    Since this DTO represents client input, and every field can be sent as null (or wrong) from client, the fields are made nullable to make this fact clear.

---
+++ src/ProductValidation.Core/Models/Dtos/ProductRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductValidation.Core.Models.Dtos;
+
+public class ProductRequestDto
+{
+    public string? Name { get; set; }
+
+    public string? Brand { get; set; }
+
+    public int? Barcode { get; set; }
+
+    public string? Description { get; set; }
+}
