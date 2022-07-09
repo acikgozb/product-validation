591b9c4d61e453540fea5f0b88eb3edf3030ad68
acikgozb <acikgozberk96@gmail.com>
2022-07-10 00:32:46 +0300

    Add remaining DTO validations
    Duplicate rules with messages will be extracted into their own extension methods later on.

--- src/ProductValidation.Core/Validators/ProductRequestDtoValidator.cs
+++ src/ProductValidation.Core/Validators/ProductRequestDtoValidator.cs
@@ -3,6 +3,7 @@ using ProductValidation.Core.Models.Dtos;
 
 namespace ProductValidation.Core.Validators;
 
+//TODO acikgozb: Create extension methods for common validation rules below.
 public class ProductRequestDtoValidator : AbstractValidator<ProductRequestDto>
 {
     public ProductRequestDtoValidator()
@@ -11,10 +12,24 @@ public class ProductRequestDtoValidator : AbstractValidator<ProductRequestDto>
             .NotEmpty()
             .WithMessage("This field cannot be empty.")
             .MinimumLength(5)
-            .WithMessage("The entered value '{PropertyValue}' must be at least {MinLength} characters.");
+            .WithMessage("The entered value '{PropertyValue}' must be at least {MinLength} characters.")
+            .Must(name => name?.All(char.IsLetter) ?? false)
+            .WithMessage("This field can only contain letters.");
 
         RuleFor(dto => dto.Barcode)
             .NotEmpty()
             .WithMessage("This field cannot be empty.");
+
+        RuleFor(dto => dto.Brand)
+            .NotEmpty()
+            .WithMessage("This field cannot be empty.")
+            .MinimumLength(3)
+            .WithMessage("The entered value '{PropertyValue}' must be at least {MinLength} characters.")
+            .Must(brand => brand?.All(char.IsLetter) ?? false)
+            .WithMessage("This field can only contain letters.");
+
+        RuleFor(dto => dto.Description)
+            .NotEmpty()
+            .WithMessage("This field cannot be empty");
     }
 }
