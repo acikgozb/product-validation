b6871fbc11f16b3d14118d404ef5e798ec47e7cb
acikgozb <acikgozberk96@gmail.com>
2022-07-09 22:42:08 +0300

    Suppress default model binding filter
    This filter runs when model binding fails (e.g type mismatch between body data and action method argument DTO). In order to use custom validation messages for type errors, this filter is disabled by using ApiBehaviorOptions.

--- src/ProductValidation.API/Program.cs
+++ src/ProductValidation.API/Program.cs
@@ -1,10 +1,14 @@
 using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using ProductValidation.API.Filters;
 using ProductValidation.Core.Contracts;
 using ProductValidation.Core.Dtos;
 using ProductValidation.Core.Services;
 using ProductValidation.Core.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
 
 builder.Services.AddControllers(options =>
 {
