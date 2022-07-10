13b953350e8f44d1b01f02d52be3a72b150d8bfe
acikgozb <acikgozberk96@gmail.com>
2022-07-11 01:33:23 +0300

    Add ProductValidator for business logic validation
    Since simple validation cases are done via DTO validator, this validator will only contain business logic validation, things like cross field validation and async validation.

---
+++ src/ProductValidation.Core/Validators/ProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ProductValidation.Core.Models;
+
+namespace ProductValidation.Core.Validators;
+
+public class ProductValidator : AbstractValidator<Product>
+{
+    public ProductValidator()
+    {
+        //Contains business model validations
+        RuleFor(p => p.Barcode)
+            .MustAsync(async (barcode, cancellationToken) =>
+            {
+                // TODO acikgozb: check if product exists for given barcode.
+                return true;
+            })
+            .WithMessage("A product with {PropertyValue} already exists.");
+    }
+}
