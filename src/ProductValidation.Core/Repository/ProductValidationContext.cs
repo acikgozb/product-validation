c9dac7810327006861a7e2b46885a6f6d0c0d393
acikgozb <acikgozberk96@gmail.com>
2022-07-11 01:29:53 +0300

    Add ProductValidationContext for database operations

---
+++ src/ProductValidation.Core/Repository/ProductValidationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductValidation.Core.Models;
+
+namespace ProductValidation.Core.Repository;
+
+public class ProductValidationContext : DbContext
+{
+    public ProductValidationContext(DbContextOptions<ProductValidationContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
