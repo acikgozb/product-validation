e5138f04e4c34627b5e3a60f1fb1703420096474
acikgozb <acikgozberk96@gmail.com>
2022-07-10 17:51:14 +0300

    Create a contract and a concrete implementation for ProductValidationService

---
+++ src/ProductValidation.Core/Contracts/IProductValidationService.cs
@@ -0,0 +1,10 @@
+﻿using ProductValidation.Core.Models;
+
+namespace ProductValidation.Core.Contracts;
+
+public interface IProductValidationService
+{
+    public List<FieldValidationResult> ValidateProduct<T>(T productModelToValidate);
+
+    public Task<List<FieldValidationResult>> ValidateProductAsync<T>(T productModelToValidate);
+}
---
+++ src/ProductValidation.Core/Services/ProductValidationService.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.Results;
+using ProductValidation.Core.Contracts;
+using ProductValidation.Core.Models;
+
+namespace ProductValidation.Core.Services;
+
+public class ProductValidationService : IProductValidationService
+{
+    private readonly IModelValidator _modelValidator;
+
+    public ProductValidationService(IModelValidator modelValidator)
+    {
+        _modelValidator = modelValidator;
+    }
+
+    public List<FieldValidationResult> ValidateProduct<T>(T productModelToValidate)
+    {
+        var validationResult = _modelValidator.Validate(productModelToValidate);
+        return ToFieldValidationResultList(validationResult);
+    }
+
+    public async Task<List<FieldValidationResult>> ValidateProductAsync<T>(T productModelToValidate)
+    {
+        var validationResult = await _modelValidator.ValidateAsync(productModelToValidate);
+        return ToFieldValidationResultList(validationResult);
+    }
+
+    private List<FieldValidationResult> ToFieldValidationResultList(ValidationResult validationResult)
+    {
+        return validationResult.Errors.ConvertAll(validationFailure =>
+            new FieldValidationResult
+            {
+                FieldName = validationFailure.PropertyName,
+                ErrorMessage = validationFailure.ErrorMessage
+            });
+    }
+}
