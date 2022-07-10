72bf9f6b563eef62e7e1a68317d6fdb31155ee31
acikgozb <acikgozberk96@gmail.com>
2022-07-10 17:52:07 +0300

    Create a contract and concrete implementation of ProductService

---
+++ src/ProductValidation.Core/Contracts/IProductService.cs
@@ -0,0 +1,8 @@
+﻿using ProductValidation.Core.Models.Dtos;
+
+namespace ProductValidation.Core.Contracts;
+
+public interface IProductService
+{
+    public Task AddProductAsync(ProductRequestDto productRequestDto);
+}
---
+++ src/ProductValidation.Core/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ProductValidation.Core.Contracts;
+using ProductValidation.Core.Extensions;
+using ProductValidation.Core.Models.Dtos;
+
+namespace ProductValidation.Core.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductValidationService _productValidationService;
+
+    public ProductService(IProductValidationService productValidationService)
+    {
+        _productValidationService = productValidationService;
+    }
+
+    public async Task AddProductAsync(ProductRequestDto productRequestDto)
+    {
+        //map to entity, then validate the entity.
+        var productEntity = productRequestDto.ToEntity();
+        var validationResult = await _productValidationService.ValidateProductAsync(productEntity);
+        throw new NotImplementedException();
+    }
+}
