9c83f9e87d8c584a9561efd47b2025d23c8a43d2
acikgozb <acikgozberk96@gmail.com>
2022-07-10 17:58:49 +0300

    Add extension method to map dto to entity manually
    AutoMapper is not used this project due to personal preference.
    
    It seemed to me like it does too much "magic", randomly written mapping profiles are picked up somehow and everything magically works.
    I will use it if its used in a team project.
    
    I prefer more control over my code, even if it means ending up with more verbose code.

---
+++ src/ProductValidation.Core/Extensions/ProductRequestDtoExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ProductValidation.Core.Models;
+using ProductValidation.Core.Models.Dtos;
+
+namespace ProductValidation.Core.Extensions;
+
+public static class ProductRequestDtoExtensions
+{
+    public static Product ToEntity(this ProductRequestDto productRequestDto)
+    {
+        return new Product
+        {
+            Name = productRequestDto.Name,
+            Barcode = productRequestDto.Barcode,
+            Brand = productRequestDto.Brand,
+            Description = productRequestDto.Description
+        };
+    }
+}
