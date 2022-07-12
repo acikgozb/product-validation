f1d1bf3a0672809acd3f64a4d439f063d620ec65
acikgozb <acikgozberk96@gmail.com>
2022-07-12 23:25:29 +0300

    Add response type info to each action in ProductsController

--- src/ProductValidation.API/Controllers/ProductsController.cs
+++ src/ProductValidation.API/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using ProductValidation.Core.Contracts;
 using ProductValidation.Core.Models.Dtos;
@@ -6,6 +7,7 @@ namespace ProductValidation.API.Controllers;
 
 [Route("api/products")]
 [ApiController]
+[Produces("application/json")]
 public class ProductsController : ControllerBase
 {
     private readonly IProductService _productService;
@@ -18,6 +20,7 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetProducts()
     {
         var products = await _productService.GetProductsAsync();
@@ -25,6 +28,8 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetProductById(int id)
     {
         var product = await _productService.GetProductByIdAsync(id);
@@ -37,7 +42,8 @@ public class ProductsController : ControllerBase
     }
 
     [HttpPost]
-    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public async Task<IActionResult> AddProduct(ProductRequestDto productRequestDto)
     {
         var validationResult = _productValidationService.ValidateProduct(productRequestDto);
