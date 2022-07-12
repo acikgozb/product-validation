21cbff3f89179835860082d543654d6e76f84e57
acikgozb <acikgozberk96@gmail.com>
2022-07-12 20:40:28 +0300

    Add migration scripts to seed BarcodeLength table and apply field type change

---
+++ src/ProductValidation.API/Migrations/20220711210603_BarcodeLengthSeeding.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProductValidation.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class BarcodeLengthSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "BarcodeLengths",
+                newName: "Value");
+
+            migrationBuilder.InsertData(
+                table: "BarcodeLengths",
+                columns: new[] { "Id", "Brand", "Value" },
+                values: new object[,]
+                {
+                    { 1, "nike", 12 },
+                    { 2, "adidas", 15 },
+                    { 3, "apple", 8 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BarcodeLengths",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "BarcodeLengths",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "BarcodeLengths",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "BarcodeLengths",
+                newName: "Length");
+        }
+    }
+}
