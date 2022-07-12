18360df1de1dcdcd295feb47ccc04a318e92068e
acikgozb <acikgozberk96@gmail.com>
2022-07-12 20:51:19 +0300

    Add remaining business logic validations for barcode and description field.

--- src/ProductValidation.Core/Validators/ProductValidator.cs
+++ src/ProductValidation.Core/Validators/ProductValidator.cs
@@ -6,10 +6,58 @@ namespace ProductValidation.Core.Validators;
 
 public class ProductValidator : AbstractValidator<Product>
 {
-    public ProductValidator(IProductDataGateway productDataGateway)
+    private readonly IProductDataGateway _productDataGateway;
+    private readonly IBarcodeLengthDataGateway _barcodeLengthDataGateway;
+    private readonly IBannedWordsDataGateway _bannedWordsDataGateway;
+    private const int DefaultRequiredBarcodeLength = 10;
+    private string? _matchedBannedWord;
+    private int _requiredBarcodeLength;
+
+    public ProductValidator(IProductDataGateway productDataGateway, IBarcodeLengthDataGateway barcodeLengthDataGateway,
+        IBannedWordsDataGateway bannedWordsDataGateway)
     {
+        _productDataGateway = productDataGateway;
+        _barcodeLengthDataGateway = barcodeLengthDataGateway;
+        _bannedWordsDataGateway = bannedWordsDataGateway;
         RuleFor(p => p.Barcode)
-            .MustAsync(async (barcode, cancellationToken) => !await productDataGateway.IsProductExistByBarcode(barcode!.Value))
-            .WithMessage("A product with {PropertyValue} barcode already exists.");
+            .MustAsync(IsBarcodeUnique)
+            .WithMessage("A product with {PropertyValue} barcode already exists.")
+            .MustAsync(DoesBarcodeLengthMatch)
+            .WithMessage(product =>
+                $"The entered barcode '{product.Barcode}' for brand '{product.Brand}' does not have required length. Its length should be {_requiredBarcodeLength} characters.");
+
+        RuleFor(p => p.Description)
+            .MustAsync(DoesBannedWordExist)
+            .WithMessage(product =>
+                $"The entered description contains a banned word '{_matchedBannedWord}', please change your product's description.");
+    }
+
+    private async Task<bool> IsBarcodeUnique(string? barcode, CancellationToken cancellationToken)
+    {
+        return !await _productDataGateway.DoesProductExistByBarcodeAsync(barcode!);
+    }
+
+    private async Task<bool> DoesBarcodeLengthMatch(Product product, string? barcode,
+        CancellationToken cancellationToken)
+    {
+        var requiredLengthByBrand = await _barcodeLengthDataGateway.GetBarcodeLengthByBrandAsync(product.Brand!);
+        _requiredBarcodeLength = requiredLengthByBrand?.Value ?? DefaultRequiredBarcodeLength;
+
+        return barcode!.Length == _requiredBarcodeLength;
+    }
+
+    private async Task<bool> DoesBannedWordExist(Product product, string? description,
+        CancellationToken cancellationToken)
+    {
+        var bannedWordsByBrand = await _bannedWordsDataGateway.GetBannedWordsByBrand(product.Brand!);
+        if (bannedWordsByBrand is null)
+        {
+            return true;
+        }
+
+        _matchedBannedWord = bannedWordsByBrand
+            .FirstOrDefault(bannedWord => description!.Contains(bannedWord.Value))?.Value;
+
+        return string.IsNullOrEmpty(_matchedBannedWord);
     }
 }
