30c0c06161e6a9aacb0733858b59ce12b7c78c72
acikgozb <acikgozberk96@gmail.com>
2022-07-12 22:54:14 +0300

    Revert if condition in ModelBindingValidationActionFilter to make it more readable

--- src/ProductValidation.API/Filters/ModelBindingValidationActionFilter.cs
+++ src/ProductValidation.API/Filters/ModelBindingValidationActionFilter.cs
@@ -8,20 +8,22 @@ public class ModelBindingValidationActionFilter : IActionFilter
 {
     public void OnActionExecuting(ActionExecutingContext context)
     {
-        if (!context.ModelState.IsValid)
+        if (context.ModelState.IsValid)
         {
-            var validationErrorResult = context.ModelState.Select(modelKeyValue =>
-            {
-                var fieldName = modelKeyValue.Key.Substring(2);
-                return new FieldValidationResult
-                {
-                    FieldName = fieldName,
-                    ErrorMessage = $"Provided value for {fieldName} is incorrect."
-                };
-            }).ToList();
-
-            context.Result = new BadRequestObjectResult(validationErrorResult);
+            return;
         }
+
+        var validationErrorResult = context.ModelState.Select(modelKeyValue =>
+        {
+            var fieldName = modelKeyValue.Key.Contains('$') ? modelKeyValue.Key.Substring(2) : modelKeyValue.Key;
+            return new FieldValidationResult
+            {
+                FieldName = fieldName,
+                ErrorMessage = $"Provided value for {fieldName} is incorrect."
+            };
+        }).ToList();
+
+        context.Result = new BadRequestObjectResult(validationErrorResult);
     }
 
     public void OnActionExecuted(ActionExecutedContext context)
