c7cd65cc6725b66f60e22f19f205d53dd1a833ce
acikgozb <acikgozberk96@gmail.com>
2022-07-13 02:59:44 +0300

    Add XML comments for ProductService

--- src/ProductValidation.Core/Services/ProductService.cs
+++ src/ProductValidation.Core/Services/ProductService.cs
@@ -20,6 +20,19 @@ public class ProductService : IProductService
         _productDataGateway = productDataGateway;
     }
 
+    /// <summary>
+    /// Converts provided DTO to Product entity, validates it and asynchronously calls target data gateway to add Product to database. 
+    /// </summary>
+    /// <param name="productRequestDto">(<c>ProductRequestDto</c>) A product that is submitted by user.</param>
+    /// <remarks>
+    /// In this method, provided DTO is converted to Product entity and then business validations are ran against it.<br/>
+    /// If any field fails its validation, a List of <c>FieldValidationResult</c> is returned back to the caller.
+    /// If all fields pass, the method proceeds to call <c>ProductDataGateway</c> to add the validated Product.
+    /// </remarks>
+    /// <returns>
+    /// List of <c>FieldValidationResult</c> - if any field is not valid.<br />
+    /// <c>Product</c> - if fields are valid.
+    /// </returns>
     public async Task<OneOf<List<FieldValidationResult>, Product>> AddProductAsync(ProductRequestDto productRequestDto)
     {
         var productEntity = productRequestDto.ToEntity();
@@ -36,11 +49,26 @@ public class ProductService : IProductService
         return addedProduct;
     }
 
+    /// <summary>
+    /// Makes a call to target data gateway to get the list of all Product's. 
+    /// </summary>
+    /// <returns>
+    /// A list of all Products. <br/>
+    /// [] <c>List</c> - if there are no Products.
+    /// </returns>
     public Task<List<Product>> GetProductsAsync()
     {
         return _productDataGateway.GetProductsAsync();
     }
 
+    /// <summary>
+    /// Makes a call to target data gateway to get the target Product by given Id. 
+    /// </summary>
+    /// <param name="Id">(<c>int</c>) Requested Product's Id.</param>
+    /// <returns>
+    /// <c>Product</c> - the corresponding Product for given Id.<br/>
+    /// <c>null</c> - if there isn't any Product with given Id.
+    /// </returns>
     public Task<Product?> GetProductByIdAsync(int id)
     {
         return _productDataGateway.GetProductByIdAsync(id);
