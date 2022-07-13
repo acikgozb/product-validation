d3b91c5779b74c5a79e14695cbacdb26383c3272
acikgozb <acikgozberk96@gmail.com>
2022-07-13 03:15:26 +0300

    Add XML comments for ProductDataGateway

--- src/ProductValidation.Core/Repository/DataGateways/ProductDataGateway.cs
+++ src/ProductValidation.Core/Repository/DataGateways/ProductDataGateway.cs
@@ -13,22 +13,51 @@ public class ProductDataGateway : IProductDataGateway
         _dbContext = dbContext;
     }
 
+    /// <summary>
+    /// Generates a database query to find out whether there is a Product created before with given barcode. 
+    /// </summary>
+    /// <param name="barcode">(<c>string</c>) unique barcode.</param>
+    /// <returns>
+    /// <c>true</c> - if there is a product exist by given barcode.<br />
+    /// <c>false</c> - if given barcode is truly unique.<br />
+    /// </returns>
     public Task<bool> DoesProductExistByBarcodeAsync(string barcode)
     {
         return _dbContext.Products.AnyAsync(product => product.Barcode == barcode);
     }
 
+    /// <summary>
+    /// Generates a database query to add given product.
+    /// </summary>
+    /// <param name="product">(<c>Product</c>) validated Product.</param>
+    /// <returns>
+    /// <c>Product</c> - Added Product that has given a unique Id.<br />
+    /// </returns>
     public Product AddProduct(Product product)
     {
         var productEntry = _dbContext.Products.Add(product);
         return productEntry.Entity;
     }
 
+    /// <summary>
+    /// Generates a database query to get the list of all products.
+    /// </summary>
+    /// <returns>
+    /// Product <c>List</c> - a list of all products.<br />
+    /// </returns>
     public Task<List<Product>> GetProductsAsync()
     {
         return _dbContext.Products.ToListAsync();
     }
 
+    /// <summary>
+    /// Generates a database query to get a product by given id.
+    /// </summary>
+    /// <param name="Id">(<c>int</c>) Requested Product's Id.</param>
+    /// <returns>
+    /// <c>Product</c> - the corresponding Product for given Id.<br/>
+    /// <c>null</c> - if there isn't any Product with given Id.
+    /// </returns>
     public Task<Product?> GetProductByIdAsync(int id)
     {
         return _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
