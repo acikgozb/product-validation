9f35357c1f3f8793bbd762837b230a3a77886cbf
acikgozb <acikgozberk96@gmail.com>
2022-07-13 03:48:18 +0300

    Add XML comments for ModelValidator

--- src/ProductValidation.Core/Services/ModelValidator.cs
+++ src/ProductValidation.Core/Services/ModelValidator.cs
@@ -13,19 +13,49 @@ public class ModelValidator : IModelValidator
         _serviceProvider = serviceProvider;
     }
 
+    /// <summary>
+    /// Synchronously validates the given model. 
+    /// </summary>
+    /// <param name="modelToValidate">A model which has a defined <c>AbstractValidator</c>.</param>
+    /// <remarks>
+    /// This method expects to get a type that has an <c>AbstractValidator</c> attached to it and injected to IoC container.<br/>
+    /// This method only does synchronous validation, if there is a need for async validation, use <c>ValidateAsync()</c>.<br/>
+    /// </remarks>
+    /// <returns>
+    /// <c>ValidationResult</c> - FluentValidation's default validation type.<br />
+    /// </returns>
     public ValidationResult Validate<T>(T modelToValidate)
     {
         var modelValidator = GetModelValidatorByType<T>();
         return modelValidator!.Validate(modelToValidate);
     }
     
-    //TODO: investigate whether it is possible to avoid using await.  
+    /// <summary>
+    /// Asynchronously validates the given model. 
+    /// </summary>
+    /// <param name="modelToValidate">A model which has a defined <c>AbstractValidator</c>.</param>
+    /// <remarks>
+    /// This method expects to get a type that has an <c>AbstractValidator</c> attached to it and injected to IoC container.<br/>
+    /// This method only does asynchronous validation, if there is a need for sync validation, use <c>Validate()</c>.<br/>
+    /// </remarks>
+    /// <returns>
+    /// <c>ValidationResult</c> - FluentValidation's default validation type.<br />
+    /// </returns>
+    //TODO: investigate whether it is possible to avoid using await.
     public async Task<ValidationResult> ValidateAsync<T>(T modelToValidate)
     {
         var modelValidator = GetModelValidatorByType<T>();
         return await modelValidator!.ValidateAsync(modelToValidate);
     }
 
+    /// <summary>
+    /// Gets the attached <c>AbstractValidator</c> to the given type.
+    /// </summary>
+    /// <typeparam name="T">A type that is also used for its corresponding <c>AbstractValidator</c></typeparam>
+    /// <returns>
+    /// T <c>IValidator</c> - FluentValidation's generic validator interface.<br />
+    /// <c>null</c> - if there is no <c>AbstractValidator</c> attached to given type.
+    /// </returns>
     private IValidator<T>? GetModelValidatorByType<T>()
     {
         return (IValidator<T>?)_serviceProvider.GetService(typeof(IValidator<T>));
