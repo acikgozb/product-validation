92af82be28823283a0e8c95d2eee4147af895ffd
acikgozb <acikgozberk96@gmail.com>
2022-07-13 03:19:42 +0300

    Add XML comments for BarcodeLengthDataGateway

--- src/ProductValidation.Core/Repository/DataGateways/BarcodeLengthDataGateway.cs
+++ src/ProductValidation.Core/Repository/DataGateways/BarcodeLengthDataGateway.cs
@@ -13,6 +13,14 @@ public class BarcodeLengthDataGateway : IBarcodeLengthDataGateway
         _dbContext = dbContext;
     }
 
+    /// <summary>
+    /// Generates a query to get the required length of barcode based on its associated brand. 
+    /// </summary>
+    /// <param name="Brand">(<c>string</c>)</param>
+    /// <returns>
+    /// <c>BarcodeLength</c> - if rule is defined in database.<br />
+    /// <c>null</c> - if there is no rule for associated brand.
+    /// </returns>
     public Task<BarcodeLength?> GetBarcodeLengthByBrandAsync(string brand)
     {
         return _dbContext.BarcodeLengths
