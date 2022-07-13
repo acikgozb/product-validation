3024d1332e73798d20a06cb94ebce12b0e937d82
acikgozb <acikgozberk96@gmail.com>
2022-07-13 03:23:08 +0300

    Add XML comments for BannedWordsDataGateway

--- src/ProductValidation.Core/Repository/DataGateways/BannedWordsDataGateway.cs
+++ src/ProductValidation.Core/Repository/DataGateways/BannedWordsDataGateway.cs
@@ -13,6 +13,14 @@ public class BannedWordsDataGateway : IBannedWordsDataGateway
         _dbContext = dbContext;
     }
 
+    /// <summary>
+    /// Generates a query to get a list of <c>BannedWords</c> based on the associated brand.
+    /// </summary>
+    /// <param name="Brand">(<c>string</c>)</param>
+    /// <returns>
+    /// BannedWord <c>List</c> - if the brand exists on DB and there are banned words linked to it.<br />
+    /// [] <c>List</c> - if the associated brand does not exist on DB or if there are no banned words.
+    /// </returns>
     public Task<List<BannedWord>?> GetBannedWordsByBrand(string brand)
     {
         return _dbContext.BannedWords
