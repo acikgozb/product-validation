6b15ef06a44a511d5afbb44f7668f54e6e5b6d8a
acikgozb <acikgozberk96@gmail.com>
2022-07-14 03:12:06 +0300

    Add unit test for ProductService.GetProductsAsync
    Figured out it is a perfect time to celebrate my first API unit test (3 AM).

---
+++ ProductValidation.UnitTests/Services/ProductServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using ProductValidation.Core.Contracts;
+using ProductValidation.Core.Models;
+using ProductValidation.Core.Services;
+using ProductValidation.UnitTests.Util.TestBase;
+
+namespace ProductValidation.UnitTests.Services;
+
+public class ProductServiceTests : TestBase<ProductService>
+{
+    [Fact]
+    public async Task GetProductsAsync_Should_Call_Product_Data_Gateway()
+    {
+        MockFor<IProductDataGateway>()
+            .Setup(dep => dep.GetProductsAsync().Result).Returns(new List<Product>());
+
+        await ClassUnderTest.GetProductsAsync();
+
+        MockFor<IProductDataGateway>()
+            .Verify(dep => dep.GetProductsAsync(), Times.Once);
+    }
+}
