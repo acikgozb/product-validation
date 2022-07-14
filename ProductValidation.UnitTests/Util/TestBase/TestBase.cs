4450212f3b8def21df79efef0f7285a9cf07cd23
acikgozb <acikgozberk96@gmail.com>
2022-07-14 03:10:06 +0300

    Add a common base test class
    To make setups easier across all test classes, a base class is written for the common functionality.
    
    This base class automatically creates mocks for all dependencies, but still allows client to configure if needed.
    
    Also, it removes the need of manually creating test instances in each method.
    
    PS: This is a simplified version of the implementation in Armut, therefore all credits go to the creative backend engineers in there.
    
    PS2: I don't know whether it is something trivial or not due to me being absolute noob in backend unit testing but still I had quite a lot of fun reading the original code. A lovely implementation really.

---
+++ ProductValidation.UnitTests/Util/TestBase/TestBase.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+
+namespace ProductValidation.UnitTests.Util.TestBase;
+
+public abstract class TestBase<TClassUnderTest>
+{
+    private readonly IFixture _fixture;
+    private readonly IDictionary<Type, Mock> _mockDictionary;
+
+    protected TestBase()
+    {
+        _mockDictionary = new Dictionary<Type, Mock>();
+        _fixture = new Fixture().Customize(new AutoMoqCustomization());
+    }
+
+    public TClassUnderTest ClassUnderTest => _fixture.Create<TClassUnderTest>();
+
+    public Mock<TDependency> MockFor<TDependency>() where TDependency : class
+    {
+        var dependencyKey = typeof(TDependency);
+        if (_mockDictionary.ContainsKey(dependencyKey))
+        {
+            return (_mockDictionary[dependencyKey] as Mock<TDependency>)!;
+        }
+
+        var mockedDependency = new Mock<TDependency>(MockBehavior.Strict);
+        _mockDictionary.Add(dependencyKey, mockedDependency);
+        _fixture.Inject(mockedDependency.Object);
+
+        return mockedDependency;
+    }
+}
